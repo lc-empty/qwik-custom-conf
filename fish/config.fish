9ef481d9a416cb072e357f6c2867016caf18038c
Isaias Ramos Lopez <155261549+qwik-zgheib@users.noreply.github.com>
2024-02-26 08:58:14 -0500

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,13 @@
-### starship configuration
+source ~/.config/fish/conf.d/alias.fish
+source ~/.config/fish/conf.d/git.fish
+source ~/.config/fish/functions/fn.fish
+
 set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
 
-### "nvim" as manpager
-set -x MANPAGER "nvim +Man!"
-
-### "less" as manpager
-# set -x MANPAGER "less"
-
-### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
 set fish_color_normal brcyan
 set fish_color_autosuggestion '#7d7d7d'
 set fish_color_command brcyan
 set fish_color_error '#ff6c6b'
 set fish_color_param brcyan
 
-### RANDOM COLOR SCRIPT ###
-# Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
-# Or install it from the Arch User Repository: shell-color-scripts
-colorscript random
-
-### SETTING THE STARSHIP PROMPT ###
 starship init fish | source
