efdb9a7eda1d4089c0957812d5a2496bdad127b2
qwik-zgheib <qwikzgheib@gmail.com>
2024-03-03 10:49:03 -0500

    modify catppuccin theme

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -2,6 +2,37 @@ return {
   "catppuccin/nvim",
   lazy = true,
   name = "catppuccin",
+  priority = 100,
+  flavour = "mocha", -- latte, frappe, macchiato, mocha
+  background = { -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = false, -- disables setting the background color.
+  show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+  term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+  dim_inactive = {
+    enabled = false, -- dims the background color of inactive window
+    shade = "dark",
+    percentage = 0.15, -- percentage of the shade to apply to the inactive window
+  },
+  no_italic = false, -- Force no italic
+  no_bold = false, -- Force no bold
+  no_underline = false, -- Force no underline
+  styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+    comments = { "italic" }, -- Change the style of comments
+    conditionals = { "italic" },
+    loops = {},
+    functions = {},
+    keywords = {},
+    strings = {},
+    variables = {},
+    numbers = {},
+    booleans = {},
+    properties = {},
+    types = {},
+    operators = {},
+  },
   opts = {
     integrations = {
       aerial = true,
@@ -15,19 +46,34 @@ return {
       indent_blankline = { enabled = true },
       leap = true,
       lsp_trouble = true,
-      mason = true,
-      markdown = true,
-      mini = true,
       native_lsp = {
         enabled = true,
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+        },
         underlines = {
-          errors = { "undercurl" },
-          hints = { "undercurl" },
-          warnings = { "undercurl" },
-          information = { "undercurl" },
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
         },
       },
-      navic = { enabled = true, custom_bg = "lualine" },
+      mason = true,
+      markdown = true,
+      mini = {
+        enabled = true,
+        indentscope_color = "",
+      },
+      navic = {
+        enabled = false,
+        custom_bg = "NONE", -- "lualine" will set background to mantle
+      },
       neotest = true,
       neotree = true,
       noice = true,
