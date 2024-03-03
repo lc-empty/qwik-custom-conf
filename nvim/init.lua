c67a2efaafa269874641664b964498033e48c631
qwik-zgheib <qwikzgheib@gmail.com>
2024-03-03 09:15:54 -0500

    nvim + catppuccin theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,2 @@
--- Leader Keys
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-
+--
 -- Autocmds
 vim.api.nvim_create_user_command("ToggleTodo", function()
   local cursor = vim.api.nvim_win_get_cursor(0)
