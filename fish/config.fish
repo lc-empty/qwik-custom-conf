8a2b968857386b5ee141605b2fe43319f9cb610f
qwik-zgheib <qwikzgheib@gmail.com>
2024-03-19 19:43:32 -0500

    add catppuccin theme in fzf

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,12 @@ source ~/.config/fish/functions/fn.fish
 set -x STARSHIP_CONFIG ~/.config/starship/starship.toml
 set -x TMUX_CONFIG ~/.config/tmux/tmux.conf
 
+# -- fzf catppuccin theme
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
 set fish_color_normal brcyan
 set fish_color_autosuggestion '#7d7d7d'
 set fish_color_command brcyan
