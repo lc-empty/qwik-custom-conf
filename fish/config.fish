81493779b6af4eb49f824bc841407355f4d3c3c7
qwik-zgheib <qwikzgheib@gmail.com>
2024-06-01 15:34:16 -0500

    update config for fish

--- fish/config.fish
+++ fish/config.fish
@@ -12,6 +12,14 @@ set -Ux FZF_DEFAULT_OPTS "\
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
+if status is-interactive
+  # Commands to run in interactive sessions can go here
+  set -x GPG_TTY (tty)
+  set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+  gpgconf --launch gpg-agent
+end
+
+
 set fish_color_normal brcyan
 set fish_color_autosuggestion '#7d7d7d'
 set fish_color_command brcyan
